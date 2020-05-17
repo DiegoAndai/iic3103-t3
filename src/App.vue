fd396f7bd21154789f2e0983f66f3b4e02b35688
DiegoAndai <dfandai@uc.cl>
2020-05-17 05:07:35 -0400

    stock graph and analytics

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <template>
-  <div id="app" class="flex flex-col min-h-screen items-center font-thin bg-gray-100 text-gray-900">
+  <div id="app" class="flex flex-col min-h-screen items-center font-thin text-gray-900">
     <img src="./assets/stonks.jpg" class="mt-2 rounded w-2/5 cursor-pointer"/>
-    stonks
+    <div v-for="stock in stocks" :key="stock.ticker">
+      <stock-info :stock="stock" />
+    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
+import moment from 'moment';
+import values from 'lodash/values';
+
+import StockInfo from './components/stock-info';
+
 export default {
-  created: function() {
+  components: {
+    StockInfo,
+  },
+  created() {
+    this.$store.dispatch('triggerExchanges');
     this.$store.dispatch('triggerStocks');
-  }
+  },
+  computed: {
+    stocks() {
+      return values(this.$store.state.stocks.stocks);
+    }
+  },
 }
 </script>
---
+++ src/components/stock-info.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="grid grid-cols-2">
+    <stock-chart :chart-data="datacollection" />
+    <div>
+      <div>{{this.stock.company_name}}</div>
+      <div>highest: {{this.highest(this.stock)}}</div>
+      <div>lowest: {{this.lowest(this.stock)}}</div>
+      <div>last: {{this.stock.values[this.stock.values.length - 1].value}}</div>
+      <div>variation: {{this.variation(this.stock)}}%</div>
+      <div>transaction volume: {{this.transactionVolume(this.stock)}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+import { max, min, sum } from 'lodash';
+
+import StockChart from './stock-chart';
+
+export default {
+  components: {
+    StockChart,
+  },
+  data() {
+    return {
+      datacollection: {}
+    }
+  },
+  props: {
+    stock: {
+      type: Object,
+      required: true,
+    }
+  },
+  methods: {
+    highest(stock) {
+      return max(stock.values.map((value) => value.value));
+    },
+    lowest(stock) {
+      return min(stock.values.map((value) => value.value));
+    },
+    variation(stock) {
+      const length = stock.values.length
+      if (length >= 2) {
+        const { value: previous} = stock.values[length - 2]
+        const { value: last} = stock.values[length - 1]
+        return (last - previous) * 100 / previous;
+      }
+    },
+    transactionVolume(stock) {
+      const bought = sum(stock.buys.map((buy) => buy.volume));
+      const sold = sum(stock.sells.map((sell) => sell.volume));
+      return bought + sold;
+    }
+  },
+  watch: {
+    stock(newStock) {
+      this.datacollection = {
+          labels: newStock.values.map((value) => moment(value.time).format('HH:mm:ss DD/MM/YYYY')),
+          datasets: [
+            {
+              label: `${this.stock.ticker}`,
+              borderColor: '#f87979',
+              showLine: true,
+              fill: false,
+              data: newStock.values.map((value) => ({ x: moment(value.time), y: value.value }))
+            }
+          ]
+      }
+    }
+  },
+}
+</script>
